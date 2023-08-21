52bad24af972771c929003f62ca5e560deb30b8e
Khaleel Ibn Ibrahim <80992588+whizkhaleel@users.noreply.github.com>
2023-08-21 03:46:49 -0700

    Update RequireAuth.js

--- src/RequireAuth.js
+++ src/RequireAuth.js
@@ -11,48 +11,10 @@ const RequireAuth = (WrappedComponent) => {
     const { token } = UseAuth();
     const [user, setUser] = useState();
     const [done, setDone] = useState();
-
-    useEffect(() => {
       
         
 
-        try {
-          const decodedToken = jwtDecode(token);
-
-          if (Date.now() >= decodedToken.exp * 1000) {
-            console.log("Token expired");
-            navigate("/login");
-          } else {
-            const userID = decodedToken.userID;
-
-            axios
-              .post(`http://127.0.0.1:3001/api/user/${userID}`)
-              .then((response) => {
-                setUser(response.data);
-                setDone(true);
-              })
-              .catch((error) => {
-                console.error("Error fetching user details:", error);
-                navigate("/login");
-              });
-          }
-        } catch (error) {
-          console.error("Token verification failed:", error);
-          navigate("/login");
-        }
-      
-    }, [navigate, token]);
-
-    if (!done) {
-      return <LoadingComponent />;
-    }
-
-    
-      return <WrappedComponent {...props} user={user} token={token} />;
-    
-      
-    
-  };
+ 
 
   return AuthenticatedComponent;
 };
