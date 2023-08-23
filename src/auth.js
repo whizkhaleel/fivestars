2abbd7f5a835335f158a3da92649e4105bcddef9
whizkhaleel <whizkhaleel@gmail.com>
2023-08-23 04:31:05 -0700

    Some Changes Made

--- src/auth.js
+++ src/auth.js
@@ -3,7 +3,7 @@ import { useState, createContext, useContext } from "react";
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState(null);
+  const [token, setToken] = useState(localStorage.getItem("Token"));
 
   return (
     <AuthContext.Provider value={{ token, setToken }}>
