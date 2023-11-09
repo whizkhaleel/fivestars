91447562829c6a5b830315e7b77defa333c9242a
whizkhaleel <whizkhaleel@gmail.com>
2023-11-09 12:18:12 +0100

    Implemented Buy Data Plan

--- src/components/includes/buyairtime/BuyAirtime.js
+++ src/components/includes/buyairtime/BuyAirtime.js
@@ -2,11 +2,29 @@ import React from "react";
 import "../panel.scss";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import LoadingComponent from "../../../LoadingComponent";
 
 const BuyAirtime = () => {
   const [network, setNetwork] = useState("");
   const [amount, setAmount] = useState("");
   const [number, setNumber] = useState("");
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [networks, setNetworks] = useState(null);
+
+  useEffect(() => {
+    axios
+      .post("/api/networks/records")
+      .then((response) => {
+        const networksInfo = response.data;
+        setNetworks(networksInfo);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching networks details:", error);
+        setIsLoading(false);
+      });
+  }, []);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -26,51 +44,65 @@ const BuyAirtime = () => {
       });
   };
   useEffect(() => {
-    document.title = "FiveStarsData | Login";
+    document.title = "FiveStarsData | Buy Airtime";
   }, []);
   return (
-    <div className="__main">
-      <div className="breadcrumb">
-        <h1>Buy Airtime</h1>
-        <div className="link">Home / Buy Airtime</div>
-      </div>
-      <div className="__panel">
-        <form method="POST" className="form-group" onSubmit={handleSubmit}>
-          <div className="input-group">
-            <label>Select network</label>
-            <select
-              name="plan"
-              defaultValue={network}
-              onChange={(event) => setNetwork(event.target.value)}
-            >
-              <option value="MTN">MTN NG</option>
-              <option value="MTN">Airtel</option>
-            </select>
+    <>
+      {isLoading === true ? (
+        <LoadingComponent />
+      ) : (
+        <div className="__main">
+          <div className="breadcrumb">
+            <h1>Buy Airtime</h1>
+            <div className="link">Home / Buy Airtime</div>
           </div>
-          <div className="input-group">
-            <label>Amount</label>
-            <input
-              type="text"
-              name="username"
-              placeholder="N0.00"
-              value={amount}
-              onChange={(event) => setAmount(event.target.value)}
-            />
+          <div className="__panel">
+            <form method="POST" className="form-group" onSubmit={handleSubmit}>
+              <div className="input-group">
+                <label>Select network</label>
+                <select
+                  name="plan"
+                  defaultValue={network}
+                  onChange={(event) => setNetwork(event.target.value)}
+                >
+                  <option value="">--- Select Network ---</option>
+                  {networks.map((network) => {
+                    return (
+                      <option key={network._id} value={network.network_id}>
+                        {network.network_name}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
+              <div className="input-group">
+                <label>Amount</label>
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="N0.00"
+                  value={amount}
+                  onChange={(event) => setAmount(event.target.value)}
+                />
+              </div>
+              <div className="input-group">
+                <label>Phone No.</label>
+                <input
+                  type="text"
+                  name="phone"
+                  placeholder="Recipient's Phone No."
+                  value={number}
+                  onChange={(event) => setNumber(event.target.value)}
+                />
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Buy
+              </button>
+            </form>
           </div>
-          <div className="input-group">
-            <label>Phone No.</label>
-            <input
-              type="text"
-              name="phone"
-              placeholder="Recipient's Phone No."
-              value={number}
-              onChange={(event) => setNumber(event.target.value)}
-            />
-          </div>
-          <button className="btn btn-primary">Buy</button>
-        </form>
-      </div>
-    </div>
+        </div>
+      )}
+    </>
   );
 };
 
